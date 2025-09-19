3d329da80fe664e9f52c38d853c545a7080bfa00
Why_me <akbaratl@yahoo.com>
2025-09-19 13:46:20 -0500

    Update setDefense.js

--- blook-rush/setDefense.js
+++ blook-rush/setDefense.js
@@ -4,13 +4,13 @@
     window.alert = n.contentWindow.alert.bind(window);
     window.prompt = n.contentWindow.prompt.bind(window);
     window.confirm = n.contentWindow.confirm.bind(window);
-    n.remove();
+    n.remove();(jssssssss😤
             function reactHandler() {
                 return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;
             };
 
             if (window.location.pathname != '/play/rush') {
-                alert('You must be in a blook rush game mode to use this hack!');
+                alert('You must be in a blook rush game mode to use this hack!'); 
             } else {
                 let e = reactHandler();
                 e.stateNode.props.firebase.setVal({
@@ -19,7 +19,17 @@
                     val: Number(parseFloat(prompt('How much defense do you want?')))
                 });
 
-                alert('Defense set!');
+                alert('Defense set!');document.onmousemove = null;
+        });
+        document.onmousemove = ((e) => {
+            e = e || window.event;
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            let top = (element.offsetTop - pos2) > 0 ? (element.offsetTop - pos2) : 0;
+            let left = (element.offsetLeft - pos1) > 0 ? (element.offsetLeft - pos1
             };
 })();
 
@@ -56,3 +66,4 @@ function footer() {
 };
 
 footer();
+
